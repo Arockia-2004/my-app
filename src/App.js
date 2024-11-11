29c86bc1e805056a802f41f6edcf2b9ab32dbcb3
Arockia-2004 <eeejarockiajasminafrina@gmail.com>
2024-11-10 20:21:59 -0800

    hi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import './App.css'; 
+import VerticalNavbar from './components/Navbar';
+ import Body from './components/Body';
+  function App() {
+     return (
+       <div className='d-flex '> <VerticalNavbar /> <Body /> </div> ); } export default App;
+
 
 function App() {
   return (
@@ -19,6 +26,7 @@ function App() {
         </a>
       </header>
     </div>
+
   );
 }
 
